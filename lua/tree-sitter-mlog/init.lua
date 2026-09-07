3efecf1525583e2061f60348cd64725a8eefe9b9
Nibato <102328+Nibato@users.noreply.github.com>
2026-09-06 23:04:02 -0700

    fix url

--- lua/tree-sitter-mlog/init.lua
+++ lua/tree-sitter-mlog/init.lua
@@ -5,7 +5,7 @@ function M.setup()
 
 	parser_config.mlog = {
 		install_info = {
-			url = "Nibato/tree-sitter-mlog.git",
+			url = "https://github.com/Nibato/tree-sitter-mlog.git",
 			files = { "src/parser.c" }, -- add "src/scanner.c" if you use an external scanner
 			branch = "main",
 		},
